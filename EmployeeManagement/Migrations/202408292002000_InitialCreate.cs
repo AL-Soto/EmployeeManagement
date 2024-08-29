1905a4bd1546be64c83ec78f015936ff08eb663a
Alexander Soto <soto1214a@hotmail.com>
2024-08-29 15:42:59 -0500

    Creacion de la base de datos

---
+++ EmployeeManagement/Migrations/202408292002000_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeManagement.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Position = c.String(),
+                        Office = c.String(),
+                        Salary = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
---
+++ EmployeeManagement/Models/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagement.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext() : base("name=MyDatabaseConnectionString")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
